1ca29bbbd9bddc56f2943f4a1c17f9692f39d542
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-10-18 15:13:10 +0100

    pyproject.toml: Use tomli

--- boexplorer/config.py
+++ boexplorer/config.py
@@ -1,7 +1,7 @@
-import tomllib
+import tomli
 
 def load_config():
     with open("boexplorer.toml", "rb") as config_file:
-        return tomllib.load(config_file)
+        return tomli.load(config_file)
 
 app_config = load_config()
